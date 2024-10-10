7558d17ade555ae16c094e4056f44df496a8d906
sanjeev <sanjeevgour729@gmail.com>
2024-10-10 23:56:47 +0530

    update code

---
+++ script.js
@@ -0,0 +1,39 @@
+// #1: Print numbers from 1 to 10
+// for(var i=1 ; i<=10; i++){ 
+//     console.log(i)
+// }
+// _________________________________________
+
+// #2: Print the odd numbers less than 100
+// for(var i=1; i<=100; i++){ 
+//     if(i % 2 !== 0){ 
+//         console.log(i)
+//     }
+// }
+// -----------------
+// for(var i=1; i<=100; i += 2){ 
+//     console.log(i)
+// }
+// _________________________________________
+
+// #3: Print the multiplication table with 7
+// for(var i=7; i<=70; i += 7){ 
+//     console.log(i)
+// }
+// -----------------
+// for(var i=1; i<=10; i++){ 
+//     var table = "7*"+ i + " = " + 7 * i;   
+//     console.log(table)
+// }
+// _________________________________________
+
+// #4: Print all the multiplication tables with numbers from 1 to 10
+for(var i=1; i<=10; i++){ 
+    console.log("table of",i)
+    for(var j=1; j<=10; j++){ 
+        console.log(`${i}*${j}=${i*j}`)
+    }
+    console.log("---------------------")
+}
+
+
