982ea4a6c33dea0de23dcab8736b32fbae69fe54
sanjeev <sanjeevgour729@gmail.com>
2024-10-11 23:57:39 +0530

    create a new file

---
+++ machineTest.js
@@ -0,0 +1,49 @@
+// interview questions (Machine Test)
+// console.log("start"); 
+// for (let i=1; i<= 10; i++){ 
+//     while(i < 10){ 
+//         console.log(i)
+//     }
+// }
+// console.log("End")
+// __________________________
+
+// const arr = [1,2,3]
+// arr[6] = 5
+// console.log(arr.length)
+// console.log(arr)
+// __________________________
+
+// const [a,b] = [10, "hii"]; //its called destructring
+// console.log(a,b) 
+// ---------------------------
+// const [,,,a] = [10,20,30,40,50]; //if we want 40 number
+// console.log(a) 
+// ---------------------------
+// const {3:a} = [10,20,30,40,50]; //if we want 40 number by indexing
+// console.log(a) 
+// __________________________
+// function abc(){ 
+//     console.log("hii");   //that is a constructor function 
+// }
+
+// const value = new abc();
+// console.log(value); 
+// __________________________
+
+// function abc(name){ 
+//     const obj = { name };
+//     return obj;                    //it is called factory function and it is another way to create object 
+//     }
+//     const value = abc("sanjeev");
+//     console.log(value); 
+// __________________________
+
+// function abc(name){                  
+//     }
+//     const value =  new abc("sanjeev");
+//     console.log(value)
+    
+//     abc.prototype.getName = function () {};
+
+// __________________________
--- script.js
+++ script.js
@@ -28,12 +28,17 @@
 // _________________________________________
 
 // #4: Print all the multiplication tables with numbers from 1 to 10
-for(var i=1; i<=10; i++){ 
-    console.log("table of",i)
-    for(var j=1; j<=10; j++){ 
-        console.log(`${i}*${j}=${i*j}`)
-    }
-    console.log("---------------------")
-}
+// for(var i=1; i<=10; i++){ 
+//     console.log("table of",i)
+//     for(var j=1; j<=10; j++){ 
+//         console.log(`${i}*${j}=${i*j}`)
+//     }
+//     console.log("---------------------")
+// }
+
+// _________________________________________
+
+
+
 
 
