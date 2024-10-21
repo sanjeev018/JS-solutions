125cce7c5c10fc750f4334380be9cc552be1ca1f
sanjeev <sanjeevgour729@gmail.com>
2024-10-21 23:38:33 +0530

    push the code

--- script.js
+++ script.js
@@ -223,42 +223,39 @@
 printPrimes(100);
 
 // Function prints the first nPrimes numbers
-function printPrimes(nPrimes)
-{
-    let n = 0;
-    let i = 2;
+// function printPrimes(nPrimes)
+// {
+//     let n = 0;
+//     let i = 2;
     
-    while(n < nPrimes)
-    {
-        if (isPrime(i))
-        {
-            console.log(n, " --> ", i);
-            n++;
-        }
+//     while(n < nPrimes)
+//     {
+//         if (isPrime(i))
+//         {
+//             console.log(n, " --> ", i);
+//             n++;
+//         }
         
-        i++;
-    }
-}
-
-function isPrime(n)
-{
-    if (n < 2)
-        return false;
+//         i++;
+//     }
+// }
+// function isPrime(n)
+// {
+//     if (n < 2)
+//         return false;
         
-    if (n == 2)
-        return true;
+//     if (n == 2)
+//         return true;
     
-    let maxDiv = Math.sqrt(n);
+//     let maxDiv = Math.sqrt(n);
     
-    for(let i = 2; i <= maxDiv; i++)
-    {
-        if (n % i == 0)
-        {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
+//     for(let i = 2; i <= maxDiv; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
 
