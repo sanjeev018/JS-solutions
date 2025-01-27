f8327f79d0432792c65b78b8b7391d53225b1fd5
sanjeev <sanjeevgour729@gmail.com>
2025-01-27 23:21:56 +0530

    add two problems

--- intermediate.js
+++ intermediate.js
@@ -131,3 +131,10 @@
 //     return console.log(i)
 // })
 // -----------------------------------------------
+// (9) Create a function that removes duplicate values from an array.
+function removeDuplicates(arr){ 
+    return [...new Set(arr)]; 
+}
+const arrayWithDuplicates = [1,2,2,3,4,4,5]; 
+const uniqueArray = removeDuplicates(arrayWithDuplicates)
+console.log(uniqueArray)
